c65a6fa98cd16b5621b80eb42dba6e0522ba8bb8
CelestifyX <152850693+CelestifyX@users.noreply.github.com>
2024-03-13 19:29:20 +0200

    Update window_manager.py

--- window_manager/window_manager.py
+++ window_manager/window_manager.py
@@ -1,19 +1,23 @@
 import os
 
 def get_window_manager():
-    if 'WM' in os.environ:
-        env_type       = 'WM'
-        window_manager = os.environ['WM']
-    elif 'XDG_CURRENT_DESKTOP' in os.environ:
-        env_type       = 'DE'
-        window_manager = os.environ['XDG_CURRENT_DESKTOP']
-    elif 'DESKTOP_SESSION' in os.environ:
-        env_type       = 'DE'
-        window_manager = os.environ['DESKTOP_SESSION']
-    else:
-        env_type = 'WM'
+    env_mapping = {
+        'WM': ['WM'],
+        'DE': ['XDG_CURRENT_DESKTOP', 'DESKTOP_SESSION']
+    }
 
-        with open(os.path.join(os.environ['HOME'], '.xinitrc'), 'r') as f:
-            lines          = f.readlines()
-            window_manager = lines[-1].split()[1]
-    return env_type, window_manager
+    for env_type, env_vars in env_mapping.items():
+        for env_var in env_vars:
+            if env_var in os.environ:
+                if env_type == 'WM':
+                    window_manager = os.environ[env_var]
+                else:
+                    window_manager = os.environ[env_var]
+
+                return env_type, window_manager
+
+    with open(os.path.join(os.environ['HOME'], '.xinitrc'), 'r') as f:
+        lines          = f.readlines()
+        window_manager = lines[-1].split()[1]
+
+    return 'WM', window_manager
