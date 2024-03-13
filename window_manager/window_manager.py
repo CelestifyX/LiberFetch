d80424bf411efcd74a28bfadf4d489d8d0b8680e
CelestifyX <152850693+CelestifyX@users.noreply.github.com>
2024-03-13 19:38:21 +0200

    Fix window_manager.py

--- window_manager/window_manager.py
+++ window_manager/window_manager.py
@@ -9,15 +9,13 @@ def get_window_manager():
     for env_type, env_vars in env_mapping.items():
         for env_var in env_vars:
             if env_var in os.environ:
-                if env_type == 'WM':
-                    window_manager = os.environ[env_var]
-                else:
-                    window_manager = os.environ[env_var]
+                return env_type, os.environ[env_var]
 
-                return env_type, window_manager
+    try:
+        with open(os.path.join(os.environ['HOME'], '.xinitrc'), 'r') as file:
+            lines          = file.readlines()
+            window_manager = lines[-1].split()[1]
 
-    with open(os.path.join(os.environ['HOME'], '.xinitrc'), 'r') as f:
-        lines          = f.readlines()
-        window_manager = lines[-1].split()[1]
-
-    return 'WM', window_manager
+            return 'WM', window_manager
+    except FileNotFoundError:
+        return 'WM', 'Unknown'
